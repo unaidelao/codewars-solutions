0010f60296929ae518edc28ae77d4467ae145467
unaidelao <unai.delao@gmail.com>
2017-10-23 14:21:13 +0200

    Add OppositeNumber.cs

---
+++ 8kyu/OppositeNumber.cs
@@ -0,0 +1,12 @@
+/*
+ * Opposite Number kata - C#
+ *
+ * Author: Unai de la O
+ */
+ 
+ using System;
+ 
+ public class OppositeNumber
+ {
+   public static int Opposite(int number) => -number;
+ }
