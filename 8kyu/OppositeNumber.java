ed6431a417cbae961b8fddc5301bd3bb591ce2a9
unaidelao <unai.delao@gmail.com>
2017-10-23 14:13:46 +0200

    Add OppositeNumber.java

---
+++ 8kyu/OppositeNumber.java
@@ -0,0 +1,12 @@
+/*
+ * Opposite number kata - java
+ *
+ * @author Unai de la O
+ */
+ 
+ public class OppositeNumber {
+   
+   public static int opposite(int number) {
+     return -number;
+   }
+ }
