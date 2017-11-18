8fa4a7a1fde365d98eda1b62d3e64ffa33d63683
unaidelao <unai.delao@gmail.com>
2017-11-18 07:57:27 +0100

    Add JennySecretMessage.java

---
+++ 8kyu/JennySecretMessage.java
@@ -0,0 +1,17 @@
+/**
+ * Jenny's Secret Message kata - Java
+ *
+ * Jenny has written a function that returns a greeting for a user. However,
+ * she's in love with Johnny, and would like to greet him slightly different.
+ *
+ * She added a special case to her function, but she made a mistake.
+ * Can you help her?
+ *
+ * @author Unai de la O
+ */
+
+  public class JennySecretMessage {
+    public static String greet(String name) {
+      return (name == "Johnny") ? "Hello, my love!" : "Hello, " + name +"!";
+    }
+  }
