b9671b7faf071940469dd34d1b03ada230021eb2
unaidelao <unai.delao@gmail.com>
2018-05-02 22:40:38 +0200

    Add EvenOrOddNumber.sql

---
+++ 8kyu/EvenOrOddNumber.sql
@@ -0,0 +1,12 @@
+/*
+ * Even or Odd kata - SQL
+ *
+ * Create a function that takes an integer as an argument and returns "Even"
+ * for even numbers or "Odd" for odd numbers.
+ *
+ * @author Unai de la O
+ */
+
+SELECT number,
+  CASE WHEN number % 2 = 0 THEN "Even" ELSE "Odd" END AS is_even
+FROM numbers
