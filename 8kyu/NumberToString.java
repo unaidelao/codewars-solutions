98640083f9df8f6b11c1ad7e6146a0e275d59ef9
unaidelao <unai.delao@gmail.com>
2018-08-30 00:13:46 +0200

    Add NumberToString.java

---
+++ 8kyu/NumberToString.java
@@ -0,0 +1,12 @@
+/**
+ * Number To String - Java
+ *
+ * We need a function that can transform a number into a string.
+ *
+ * @author Unai de la O
+ */
+class NumberToString {
+    public static String numberToString(int num) {
+        return num + "";
+    }
+}
