bdff100c57d9bee88809a961118ea2ae94e65b07
unaidelao <unai.delao@gmail.com>
2018-09-01 01:21:58 +0200

    Add GreetHelloWorld.java

---
+++ 8kyu/GreetHelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Funtion 1 Hello World - Java
+ * 
+ * Make a simple function called greet that returns the most-famous "hello world!".
+ * 
+ * @author Unai de la O
+ */
+public class GreetHelloWorld {
+    public static String greet() {
+        return "hello world!";
+    }
+}
