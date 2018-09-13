a3811567671246f75b3136b459e6c78c9cbfc246
unaidelao <unai.delao@gmail.com>
2018-09-14 01:00:08 +0200

    Add ByteSum.cs

---
+++ 8kyu/ByteSum.cs
@@ -0,0 +1,15 @@
+/*
+    Vasya Pupkin just started learning Java and C#. At first, he decided to write simple program
+    that was supposed to sum up two small numbers (numbers and their sum fit in a byte), but it
+    didn't work. Vasya was too sad to find out what is wrong. Help him to correct the mistake.
+
+    Author: Unai de la O
+ */
+public class ByteSum 
+{
+    public static byte sum (byte a, byte b) 
+    {
+        byte c = (byte) (a + b); // needed a byte casting.
+        return c;
+    }
+}
