ee99ed49ce0c7a511e707696bed83afcf42e1748
unaidelao <unai.delao@gmail.com>
2018-09-16 08:24:01 +0200

    Add SumOfOddNumbers.java

---
+++ 7kyu/SumOfOddNumbers.java
@@ -0,0 +1,13 @@
+/**
+ * Sum of odd numbers - Java
+ * 
+ * Given the triangle of consecutive odd numbers, Calculate the row sums of this triangle from the
+ * row index (starting at index 1).
+ * 
+ * @author Unai de la O
+ */
+class RowSumOddNumbers {
+    public static int rowSumOddNumbers(int n) {
+        return n * n * n;
+    }
+}
