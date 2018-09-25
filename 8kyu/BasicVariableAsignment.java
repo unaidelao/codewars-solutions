6cb0d5aa6acfdee49f82c29e11724ea4dfb13d45
unaidelao <unai.delao@gmail.com>
2018-09-26 00:51:15 +0200

    Add BasicVariableAsignment.java

---
+++ 8kyu/BasicVariableAsignment.java
@@ -0,0 +1,11 @@
+/**
+ * Basic variable asignment - Java
+ * 
+ * This code should store "codewa.rs" as a variable called "name" but it's not working. Can you
+ * figure out why?
+ * 
+ * @author Unai de la O
+ */
+public class Kata {
+    public static String name = "codewa.rs";
+}
