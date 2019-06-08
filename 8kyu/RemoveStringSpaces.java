247a8998d4e5df2e93e609a90ed406adffcde170
unaidelao <unai.delao@gmail.com>
2019-06-08 07:43:42 +0200

    Add RemoveStringSpaces.java

---
+++ 8kyu/RemoveStringSpaces.java
@@ -0,0 +1,12 @@
+/**
+ * Remove String Spaces - Java
+ * 
+ * Simple, remove the spaces from the string, then return the resultant string.
+ * 
+ * @author Unai de la O
+ */
+class Kata {
+    static String noSpace(final String x) {
+        return x.replace(" ", "");
+    }
+}
