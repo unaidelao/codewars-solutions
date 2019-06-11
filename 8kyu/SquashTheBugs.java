a18e9a85e31c48bf4b134388f6967371f06c8be1
unaidelao <unai.delao@gmail.com>
2019-06-11 07:05:33 +0200

    Add SquashTheBugs.java

---
+++ 8kyu/SquashTheBugs.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+/**
+ * Squash the bugs - Java
+ * 
+ * Simple challenge - eliminate all bugs from the supplied code so that the code
+ * runs and outputs the expected value.
+ * 
+ * Output should be the length of the longest word, as a number.
+ * 
+ * @author Unai de la O
+ */
+public class Kata {
+
+    public static int findLongest(String str) {
+        String[] spl = str.split(" ");
+        int longest = 0;
+        for (int i = 0; i < spl.length; i++) {
+            if (spl[i].length() > longest) {
+                longest = spl[i].length();
+            }
+        }
+        return longest;
+    }
+}
