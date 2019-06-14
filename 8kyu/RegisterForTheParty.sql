763013cd615e08c720228bc57d6d2a511174597c
unaidelao <unai.delao@gmail.com>
2019-06-14 05:17:29 +0200

    Add RegisterForTheParty.sql

---
+++ 8kyu/RegisterForTheParty.sql
@@ -0,0 +1,24 @@
+/*
+REGISTER FOR THE PARTY - SQL
+
+You received an invitation to an amazing party. Now you need to write an insert statement to add
+yourself to the table of participants.
+
+== participants table schema ==
+    name (string)
+    age (integer)
+    attending (boolean)
+
+NOTES:
+
+- Since alcohol will be served, you can only attend if you are 21 or older.
+- You can't attend if the attending column returns anything but true.
+- Don't remove the 'SELECT' command from the solution!
+- Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
+
+Author: Unai de la O.
+*/
+INSERT INTO participants (name, age, attending)
+VALUES ('John', 29, true);
+
+SELECT * FROM participants;
