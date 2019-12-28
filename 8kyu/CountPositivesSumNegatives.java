bca73445e69480e0b427f95346bb5c86ee7f8ef0
unaidelao <unai.delao@gmail.com>
2019-12-28 14:00:24 +0100

    Refactor class name

--- 8kyu/CountPositivesSumNegatives.java
+++ 8kyu/CountPositivesSumNegatives.java
@@ -3,12 +3,12 @@
  * 
  * Given an array of integers.
  * 
- * Return an array, where the first element is the count of positives numbers and the second
- * element is sum of negative numbers.
+ * Return an array, where the first element is the count of positives numbers
+ * and the second element is sum of negative numbers.
  * 
  * @author Unai de la O
  */
-public class Kata {
+public class CountPositivesSumNegatives {
     public static int[] countPositivesSumNegatives(int[] input) {
         if (input == null || input.length == 0) {
             return new int[] {};
@@ -17,13 +17,12 @@ public class Kata {
         int sum = 0;
         for (int num : input) {
             if (num > 0) {
-            count++;
-        }
-            else {
+                count++;
+            } else {
                 sum += num;
             }
         }
-      
-        return new int[] {count, sum};
+
+        return new int[] { count, sum };
     }
-  }
+}
