b7e4b99edc3de99cfcceb68bfb6e2951f4f62d66
unaidelao <unai.delao@gmail.com>
2019-12-28 13:56:48 +0100

    Refactor class name

--- 8kyu/StringRepeat.java
+++ 8kyu/StringRepeat.java
@@ -1,11 +1,12 @@
 /**
  * String Repeat - Java
  * 
- * Write a function called repeatStr which repeats the given string string exactly n times.
+ * Write a function called repeatStr which repeats the given string string
+ * exactly n times.
  * 
  * @author Unai de la O
  */
-public class Solution {
+public class StringRepeat {
     public static String repeatStr(final int repeat, final String string) {
         String result = "";
         for (int i = 0; i < repeat; i++) {
