a79dc3020bfdb15b545d963d4473ec3795e5faba
unaidelao <unai.delao@gmail.com>
2019-12-28 14:02:02 +0100

    Refactor class name

--- 8kyu/BasicVariableAsignment.java
+++ 8kyu/BasicVariableAsignment.java
@@ -1,11 +1,11 @@
 /**
  * Basic variable asignment - Java
  * 
- * This code should store "codewa.rs" as a variable called "name" but it's not working. Can you
- * figure out why?
+ * This code should store "codewa.rs" as a variable called "name" but it's not
+ * working. Can you figure out why?
  * 
  * @author Unai de la O
  */
-public class Kata {
+public class BasicVariableAsignment {
     public static String name = "codewa.rs";
 }
